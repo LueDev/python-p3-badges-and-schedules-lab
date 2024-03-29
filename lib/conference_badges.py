b4a77396574f29bf66b3360f49b5f578037028a7
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-28 20:36:48 -0400

    All tests passing. Using list comprehension for all functions to build faster

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,15 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {index +1}!" for index,name in enumerate(names) if index < 8]
 
 def printer(names):
-    return None
+    [print(x) for x in batch_badge_creator(names)]
+    [print(x) for x in assign_rooms(names)]
+
+if __name__ == "__main__":
+    printer(["simon", "simon"])
